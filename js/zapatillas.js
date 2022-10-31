92c049cca47e2cc68ae1d5e5d584681a218b3670
rodcar <ivanrod12@hotmail.com>
2022-10-30 21:16:08 -0500

    cambia a bordes redondeados

--- js/zapatillas.js
+++ js/zapatillas.js
@@ -8,14 +8,14 @@ async function cargarJSON() {
         <div class="u-align-center u-container-style u-products-item u-repeater-item"">
             <div class="u-container-layout u-similar-container u-valign-bottom-sm u-container-layout-1"><!--product_image-->
             <a href="descripcion.html?id=${producto.id}">
-                <img alt="" class="u-expanded-width u-image u-image-default u-product-control u-image-1" src="${producto.img}" data-image-height="385"><!--/product_image-->
+                <img alt="" class="u-expanded-width u-image u-image-default u-product-control u-image-1 rounded" src="${producto.img}" data-image-height="385"><!--/product_image-->
             </a>   
-            <div class="u-align-center u-container-style u-grey-10 u-group u-group-1">
+            <div class="u-align-center u-container-style u-grey-10 u-group u-group-1 rounded">
                 <div class="u-container-layout u-valign-bottom u-container-layout-2">
                 <h4 class="u-text u-text-default u-text-font u-text-1">${producto.nombre}<br>
                 </h4>
                 <h6 class="u-text u-text-default u-text-2">S/.${producto.precio}</h6><!--product_button--><!--options_json--><!--{"clickType":"add-to-cart","content":"aGREGAR AL CARRITO"}--><!--/options_json-->
-                <a href="#" class="u-border-2 u-border-grey-25 u-btn u-btn-rectangle u-button-style u-palette-2-base u-product-control u-text-body-color u-btn-1"><!--product_button_content-->aGREGAR AL CARRITO<!--/product_button_content--></a><!--/product_button-->
+                <a href="#" class="u-border-2 u-border-grey-25 u-btn rounded u-button-style u-palette-2-base u-product-control u-text-body-color u-btn-1"><!--product_button_content-->AGREGAR AL CARRITO<!--/product_button_content--></a><!--/product_button-->
                 </div>
             </div>
             </div>
