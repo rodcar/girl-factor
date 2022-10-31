6eba88bb6fbc566721719c04bfac0bc60fa1cb77
rodcar <ivanrod12@hotmail.com>
2022-10-31 10:04:45 -0500

    fix responsive lista de productos del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -24,26 +24,26 @@ function loadShoppingCart() {
       <li class="list-group-item">
       <div class="container-fluid">
           <div class="row">
-              <div class="col-2 d-flex align-items-center">
+              <div class="col-sm-2 d-flex align-items-center justify-content-center">
                   <img src="${producto.img}" alt="item" width="100" class="rounded">
               </div>
-              <div class="col-3 d-flex align-items-center">
-                  <div class="container">
+              <div class="col-sm-3 d-flex align-items-center justify-content-center">
+                  <div class="container text-center">
                           <p class="mb-0">${producto.nombre}</p>
                           <p class="fw-bold">${producto.marca}</p> 
                   </div>
               </div>
-              <div class="col-3 d-flex align-items-center">
-                  <div class="container">
+              <div class="col-sm-3 d-flex align-items-center justify-content-center">
+                  <div class="container text-center">
                           <p class="mb-0">S/.${producto.precio}</p>
                   </div>
               </div>                                
-              <div class="col-2 d-flex align-items-center">
+              <div class="col-sm-2 d-flex align-items-center justify-content-center">
                   <input type="button" value="-" class="button-minus border rounded-circle  icon-shape icon-sm mx-1 " data-field="quantity" onclick="changeItemQuantity(${producto.id}, -1)">
                   <span class="w-25 text-center">${producto.cantidad}</span>
                   <input type="button" value="+" class="button-plus border rounded-circle icon-shape icon-sm lh-0" data-field="quantity" onclick="changeItemQuantity(${producto.id}, 1)">
               </div>
-              <div class="col-2 d-flex align-items-center"><a href="#" class="text-decoration-none" data-id="${producto.id}" onclick="deleteItem(${producto.id})">Eliminar</a></div>
+              <div class="col-sm-2 d-flex align-items-center justify-content-center"><a href="#" class="text-decoration-none" data-id="${producto.id}" onclick="deleteItem(${producto.id})">Eliminar</a></div>
           </div>
       </div>
   </li>
